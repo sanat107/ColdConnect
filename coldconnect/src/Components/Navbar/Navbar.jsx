06bf9f237a7ba9cf9a647982ad4bc378df45e4ee
karthik <karthiknadar1204@gmail.com>
2023-06-01 11:55:57 +0530

    New App

---
+++ coldconnect/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+                <li>home</li>
+            </ul>
+
+            <button>Login</button>
+            <button>Sign up</button>
+        </nav>
+
+
+    </div>
+  )
+}
+
+export default Navbar
