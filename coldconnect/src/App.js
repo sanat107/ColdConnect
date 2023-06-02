0835ce508ff712e3ec9169891cb89bd5dfc538fc
karthik <karthiknadar1204@gmail.com>
2023-06-02 12:14:20 +0530

    routes

--- coldconnect/src/App.js
+++ coldconnect/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./Components/Login/Login.jsx"
+import Signup from "./Components/Signup/Signup.jsx"
+import Home from "./Components/Home/Home.jsx"
+import GenerateMail from './Components/GenerateMail/GenerateMail.jsx';
+import GenerateResume from './Components/GenerateResume/GenerateResume.jsx';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-
-
+        <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={ <Login/>  }/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path='/generatemail' element={ <GenerateMail/> }></Route>
+        <Route path='/generatResume' element={ <GenerateResume/> }></Route>
+        
+      </Routes>
+  </BrowserRouter>
+      
+      
     </div>
   )
 }
--- coldconnect/src/Components/Navbar/Navbar.jsx
+++ coldconnect/src/Components/Navbar/Navbar.jsx
@@ -2,19 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        <nav>
-            <ul>
-                <li>how it works</li>
-                <li>our team</li>
-            </ul>
-
-            <button>Login</button>
-            <button>Sign up</button>
-        </nav>
-        <button>Get Started</button>
-
-    </div>
+    <>
+    Navbar
+    </>
   )
 }
 
