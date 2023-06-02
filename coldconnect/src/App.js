2a2af3dffa385017fc30f3482b56caf7843d742c
John Doe <tirunaharipandu@gmail.com>
2023-06-02 12:22:17 +0530

    kruthardh first

--- coldconnect/src/App.js
+++ coldconnect/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-
-
+      <Footer/>
     </div>
   )
 }
